e00b8eddccd7567f27705dad3c5b64b20f8a43f3
J-Rios <jrios.github@gmail.com>
2021-01-17 14:59:33 +0100

    Added sources.

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# minbasecli
+# MinBaseCLI
+
 A minimal Command Line Interface C++ library implementation with HAL emphasis to be used in different kind of devices and frameworks.
+
+## Supported Devices & Frameworks
+
+Currently, just Arduino devices are compatible, however is simply to add support to other devices and frameworks.
+
+## Adding new Device Support
+
+To add a new device or framework support to library, yo just need to write the low level functionalities to control interface of the CLI in the **hal_*()** private methods (*hal_iface_available(), hal_iface_read(), hal_iface_print(), etc.*).
+
+```cpp
+
+// ...
+
+// Device/Framework Libraries
+#if defined(ARDUINO)
+    #include <Arduino.h>
+#elif defined(ESP_IDF)
+    // Unimplemented
+#elif defined(SAM_ASF)
+    // Unimplemented
+#elif defined(__AVR__)
+    // Unimplemented
+#endif
+
+// ...
+
+uint8_t MINBASECLI::hal_iface_read(void)
+{
+    // Do nothing if interface has not been initialized
+    if(iface_is_not_initialized())
+        return 0;
+
+    #if defined(ARDUINO)
+        #if defined(USBCON) // Arduinos: Leonardo, Micro, MKR, etc
+            Serial_* _Serial = (Serial_*)this->iface;
+        #else // Arduinos: UNO, MEGA, Nano, etc
+            HardwareSerial* _Serial = (HardwareSerial*)this->iface;
+        #endif
+        return _Serial->read();
+    #elif defined(ESP_IDF)
+        // Unimplemented
+    #elif defined(SAM_ASF)
+        // Unimplemented
+    #elif defined(__AVR__)
+        // Unimplemented
+    #endif
+
+    return 0;
+}
+```
+
+## Donation
+
+[![paypal](https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif)](https://paypal.me/josrios)
+
+Do you like this project?
+
+Please, considere making a donation.
---
+++ examples/arduino/basic_usage/basic_usage.ino
@@ -0,0 +1,146 @@
+/**************************************************************************************************/
+// File: basic_usage.ino
+// Description: MINBASECLI library basic usage example
+// Date: 06 jan. 2021
+// Version: 1.0.0
+/**************************************************************************************************/
+/**
+ * @file    basic_usage.ino
+ * @author  Jose Miguel Rios Rubio <jrios.github@gmail.com>
+ * @date    17-01-2021
+ * @version 1.0.0
+ *
+ * @section DESCRIPTION
+ *
+ * SIMPLECLI library basic usage example.
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2021 Jose Miguel Rios Rubio. All right reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/*****************************************************************************/
+
+/* Libraries */
+
+// Device Framework libraries
+#include <Arduino.h>
+
+// Custom libraries
+#include <minbasecli.h>
+
+/*****************************************************************************/
+
+/* Defines, Macros, Constants and Types */
+
+// Serial Speed
+#define SERIAL_BAUDS 115200
+
+// LED to be controlled thorugh CLI command
+#define COMMAND_LED 13
+
+// Current Firmware Version
+#define FW_VER "1.0.0"
+
+/*****************************************************************************/
+
+/* Global Elements */
+
+// Command Line Interface
+MINBASECLI Cli;
+
+/*****************************************************************************/
+
+/* Setup & Loop */
+
+void setup()
+{
+    // Serial Init
+    Serial.begin(SERIAL_BAUDS);
+
+    // LED Init
+    digitalWrite(COMMAND_LED, LOW);
+    pinMode(COMMAND_LED, OUTPUT);
+
+    // CLI init to use Serial as interface
+    Cli.setup(&Serial);
+}
+
+void loop()
+{
+    t_cli_result cli_read;
+
+    // If any command was received
+    if(Cli.manage(&cli_read))
+    {
+        // Show read result element
+        Serial.print("Command received: "); Serial.println(cli_read.cmd);
+        Serial.print("Number of arguments: "); Serial.println(cli_read.argc);
+        for(uint8_t i = 0; i < cli_read.argc; i++)
+        {
+            Serial.print("    Argument "); Serial.print(i);
+            Serial.print(":"); Serial.println(cli_read.argv[i]);
+        }
+        Serial.println();
+
+        // Handle Commands
+        if(strcmp(cli_read.cmd, "help") == 0)
+        {
+            Serial.println("Available Commands:");
+            Serial.println("  help - Current info.");
+            Serial.println("  led [on/off] - Turn LED ON or OFF");
+            Serial.println("  version - Shows current firmware version");
+        }
+        else if(strcmp(cli_read.cmd, "led") == 0)
+        {
+            bool invalid_argv = false;
+            char* led_mode = NULL;
+
+            // Check for argument
+            if(cli_read.argc == 0)
+                invalid_argv = true;
+            else
+            {
+                led_mode = cli_read.argv[0];
+                if(strcmp(led_mode, "on") == 0)
+                {
+                    Serial.println("Turning LED ON.");
+                    digitalWrite(COMMAND_LED, HIGH);
+                }
+                else if(strcmp(led_mode, "off") == 0)
+                {
+                    Serial.println("Turning LED OFF.");
+                    digitalWrite(COMMAND_LED, LOW);
+                }
+                else
+                    invalid_argv = true;
+            }
+            
+            if(invalid_argv)
+                Serial.println("led command needs \"on\" or \"off\" arg.");
+        }
+        else if(strcmp(cli_read.cmd, "version") == 0)
+        {
+            Serial.print("Fw Version: ");
+            Serial.println(FW_VER);
+        }
+        // ...
+        else
+            Serial.println("Unkown command.");
+        Serial.println();
+    }
+}
